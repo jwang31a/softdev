3e9c2ca78c8109c19e0022f50b884208874e159f
Your Name <you@example.com>
2022-10-10 20:06:23 -0400

    html

--- 08_serve/v3/app.py
+++ 08_serve/v3/app.py
@@ -1,4 +1,4 @@
-# Clyde 'Thluffy' Sinclair
+# CDDN - Jeremy Kwok, Brianna Toe, Jun Hong
 # SoftDev
 # Oct 2022
 
@@ -13,3 +13,49 @@ def hello_world():
 
 app.debug = True
 app.run()
+
+'''
+Expected Behaviors:
+    -On the initial run: 
+        -Prints the same message as v0 did up, except "Debug mode: on"
+    
+    -After the "Press CTRL+C to quit line":
+        -Prints 
+
+            about to print __name__...
+            __main__
+            127.0.0.1 - - [Date/Month_Abbreviation/Year Hour:Minute:Second] "GET / HTTP/1.1" 200 -
+
+        in the terminal
+
+Actual Behaviors:
+    -Initial run message printed to the terminal:
+
+         * Serving Flask app 'app'
+         * Debug mode: on
+        WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
+         * Running on http://127.0.0.1:5000
+        Press CTRL+C to quit
+         * Restarting with stat
+         * Debugger is active!
+         * Debugger PIN: 103-671-524
+
+    has 3 new lines (the bottom three)
+
+    -After each reload of the website:
+        
+        about to print __name__...
+        __main__
+        127.0.0.1 - - [Date/Month_Abbreviation/Year Hour:Minute:Second] "GET / HTTP/1.1" 200 -
+    
+    is printed into the terminal
+
+    -After any change and save to the file (even if it's a comment):
+
+         * Detected change in '/Users/jeremykwok/Desktop/Softdev/00_flask/v3/app.py', reloading
+         * Restarting with stat
+         * Debugger is active!
+         * Debugger PIN: 103-671-524
+    
+    is printed into the terminals
+'''
--- 08_serve/v4/app.py
+++ 08_serve/v4/app.py
@@ -1,4 +1,4 @@
-# Clyde 'Thluffy' Sinclair
+# CDDN - Jeremy Kwok, Brianna Toe, Jun Hong
 # SoftDev
 # Oct 2022
 
@@ -14,3 +14,59 @@ def hello_world():
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
     app.run()
+
+'''
+Expected Behaviors:
+
+    -Initial run message printed to the terminal:
+
+         * Serving Flask app 'app'
+         * Debug mode: on
+        WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
+         * Running on http://127.0.0.1:5000
+        Press CTRL+C to quit
+         * Restarting with stat
+         * Debugger is active!
+         * Debugger PIN: 103-671-524
+        
+    -After the "Press CTRL+C to quit line" on each run:
+        -Prints 
+
+            the __name__ of this module is......
+            __main__
+            127.0.0.1 - - [Date/Month_Abbreviation/Year Hour:Minute:Second] "GET / HTTP/1.1" 200 -
+
+        in the terminal
+
+    -After any change and save to the file (even if it's a comment):
+
+         * Detected change in '/Users/jeremykwok/Desktop/Softdev/00_flask/v4/app.py', reloading
+         * Restarting with stat
+         * Debugger is active!
+         * Debugger PIN: 103-671-524
+    
+    is printed into the terminals
+
+Actual Behaviors:
+
+    -Website looks exactly the same as all the other versions
+    -Prints the expected initial run message
+    -On each run:
+        -Prints 
+
+            the __name__ of this module is......
+            __main__
+            127.0.0.1 - - [Date/Month_Abbreviation/Year Hour:Minute:Second] "GET / HTTP/1.1" 200 -
+
+        in the terminal
+    
+    -After any change and save:
+
+         * Detected change in '/Users/jeremykwok/Desktop/Softdev/00_flask/v4/app.py', reloading
+         * Restarting with stat
+         * Debugger is active!
+         * Debugger PIN: 103-671-524
+    
+    is printed into the terminal
+
+'''
