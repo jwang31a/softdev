fd6afb40c4e6e0dad0efb66f8a670561bfa07218
Jun Hong Wang <jwang31@stuy.edu>
2023-04-04 14:02:22 -0400

    asd

---
+++ 27_js/fibfac.js
@@ -0,0 +1,24 @@
+// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon 
+// SoftDev pd0
+// K27 -- Basic functions in JavaScript
+// 2023-04-04t
+// --------------------------------------------------
+
+
+// as a duo...
+// pair programming style,
+// implement a fact and fib fxn
+
+var fact = function(n) {
+  if (n < 2) {
+    return 1;
+  } else {
+    return n * fact(n - 1);
+  }
+}
+
+var 
+
+//Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
+
+console.log(fact(5));
