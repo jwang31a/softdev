e20fc95235288e9258fbaf09909d6884dedd2e91
Your Name <you@example.com>
2023-05-30 20:08:19 -0400

    jimpact ajax

---
+++ ajax/static/js/ajax.js
@@ -0,0 +1,14 @@
+//this doesn't do anything, copy pasted code in script tag in index.html
+var loadDoc = function() {
+    var xhttp = new XMLHttpRequest();
+    //i have no idea what is in this request
+    //console.log(xhttp);
+    
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("aj").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "static/ajax_info.txt", true);
+    xhttp.send();
+}
